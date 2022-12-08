3fa5839c8e6f8ed652b95f7bc178f8b32449d41f
bsrdjan <srdjan.boskovic@sap.com>
2022-12-08 09:35:41 +0100

    back() added to dashboard page

--- src/components/dashboard-pages/dashboard-home.ts
+++ src/components/dashboard-pages/dashboard-home.ts
@@ -1,5 +1,10 @@
-import { IRouteableComponent } from '@aurelia/router';
+import { IRouteableComponent, IRouter } from '@aurelia/router';
 
 export class DashboardHome implements IRouteableComponent {
+    constructor(@IRouter readonly router: IRouter) {
+    }
 
+    back() {
+        this.router.back();
+    }
 }
